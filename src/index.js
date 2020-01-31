9b2879dabbdf2160f1b38a8372342a1eab9910e8
HXXNS <39939869+hoonn9@users.noreply.github.com>
2020-01-31 22:07:13 +0900

    #2.2

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 
-const useTitle = initialTitle => {
-  const [title, setTitle] = useState(initialTitle);
-  const updateTitle = () => {
-    const htmlTitle = document.querySelector("title");
-    htmlTitle.innerText = title;
-  };
-  useEffect(updateTitle, [title]);
-  return setTitle;
+const useClick = onClick => {
+  if (typeof onClick !== "function") {
+    return;
+  }
+  const element = useRef();
+  useEffect(() => {
+    //did mount
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    //will unMount
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, []);
+  return element;
 };
 
 const App = () => {
-  const titleUpdater = useTitle("Loading");
-  setTimeout(() => titleUpdater("Home"), 5000);
+  const sayHello = () => console.log("hello");
+  const title = useClick(sayHello);
   return (
     <div className="App">
-      <div>Hi</div>
+      <h1 ref={title}>Hi</h1>
     </div>
   );
 };
